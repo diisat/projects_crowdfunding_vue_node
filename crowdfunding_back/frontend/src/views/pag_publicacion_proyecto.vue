6d4dae6e56aee13f7b2f908324cdf07e3c6bacd8
Jose Galvis <jagn1997@hotmail.com>
2020-05-18 17:04:04 -0500

    publicacion de proyecto mas avances

--- crowdfunding_back/frontend/src/views/pag_publicacion_proyecto.vue
+++ crowdfunding_back/frontend/src/views/pag_publicacion_proyecto.vue
@@ -1,32 +1,33 @@
 <template>
   <div class="margen">
+    
     <h2 class="titulo">PUBLICACIÓN PROYECTO</h2>
     <v-text-field class="margenCampos" v-model="nombre" label="Nombre"></v-text-field>
     <v-text-field class="margenCampos" v-model="descripcion" label="Descripcion"></v-text-field>
     <v-menu offset-y>
-            <template v-slot:activator="{ on }">
-              <v-btn class="margenCampos" v-on="on">{{categoria}}</v-btn>
-            </template>
-            <v-list>
-              <v-list-item
-                v-for="(cate, index) in categorias"
-                :key="index"
-                @click="cambiarCategoria(cate)"
-              >
-                <v-list-item-title>{{ cate.titulo }}</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
+      <template v-slot:activator="{ on }">
+        <v-btn class="margenCampos" v-on="on">{{categoria}}</v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="(cate, index) in categorias"
+          :key="index"
+          @click="cambiarCategoria(cate)"
+        >
+          <v-list-item-title>{{ cate.titulo }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
     <v-text-field class="margenCampos" v-model="dineroNecesario" label="Dinero Necesario"></v-text-field>
     <v-text-field class="margenCampos" v-model="vigencia" label="Vigencia (días)"></v-text-field>
     <v-text-field class="margenCampos" v-model="cuentaBancaria" label="Cuenta Bancaria"></v-text-field>
 
-    <v-btn  @click="publicar" type="submit" color="#A51F1F" dark class="titulo">PUBLICAR</v-btn>
+    <v-btn @click="publicar" type="submit" color="#A51F1F" dark class="titulo">PUBLICAR</v-btn>
   </div>
 </template>
 
 <script>
-import axios from '../plugins/axios'
+import axios from "../plugins/axios";
 export default {
   data() {
     return {
@@ -40,7 +41,7 @@ export default {
         { titulo: "Deporte" },
         { titulo: "Educación" },
         { titulo: "Tecnología" },
-        { titulo: "Arte" },
+        { titulo: "Arte" }
       ]
     };
   },
@@ -54,9 +55,9 @@ export default {
         this.cuentaBancaria == "" ||
         this.categoria == "Escoja la categoría"
       ) {
-        alert("Complete todos los campos")
-      }else{
-        let proyecto ={
+        alert("Complete todos los campos");
+      } else {
+        let proyecto = {
           nombre: this.nombre,
           descripcion: this.descripcion,
           dineroNecesario: this.dineroNecesario,
@@ -66,19 +67,17 @@ export default {
           categoria: this.categoria,
           contribuyentes: [],
           idCreador: "5ec2f073db56da355cdab2ca"
-        }
-        axios.post("/proyecto", proyecto).then(response =>{
-          if(response.data == "Proyecto creado satisfactoriamente"){
-            alert(response.data)
+        };
+        axios.post("/proyecto", proyecto).then(response => {
+          if (response.data == "Proyecto creado satisfactoriamente") {
+            this.resultadoBueno = true;
             this.$router.push("/proyectos");
           }
-        })
-
-        
+        });
       }
     },
-    cambiarCategoria(cat){
-      this.categoria=cat.titulo;
+    cambiarCategoria(cat) {
+      this.categoria = cat.titulo;
     }
   }
 };
@@ -96,7 +95,7 @@ export default {
   margin-bottom: 5%;
 }
 
-.margenCampos{
+.margenCampos {
   margin-top: 3%;
 }
 </style>
