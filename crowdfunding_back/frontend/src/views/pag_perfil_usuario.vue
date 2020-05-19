ef5c0daf2fa908de26e26757468ec20f39594da9
dianaitr <dianatorresrojas@hotmail.com>
2020-05-19 17:15:38 -0500

    Editar perfil hecho

--- crowdfunding_back/frontend/src/views/pag_perfil_usuario.vue
+++ crowdfunding_back/frontend/src/views/pag_perfil_usuario.vue
@@ -1,44 +1,22 @@
 <template>
-<div>
-  <v-card
-    class="mx-auto"
-    max-width="80%"
-    color="#C62828"
-    tile
-  >
-    
-      <v-row
-        align="center"
-        class="fill-height"
-      >
-
-        <v-col
-          align-self="start"
-          cols="3"
-        >
-        <!-- <v-col
+  <div>
+    <v-card class="mx-auto" max-width="80%" color="#C62828" tile>
+      <v-row align="center" class="fill-height">
+        <v-col align-self="start" cols="3">
+          <!-- <v-col
           align-self="start"
           class="pa-0"
           cols="12"
-        > -->
-        <!-- AVATAR -->
-          <v-avatar
-            class="profile"
-            color="grey"
-            size="170"
-            tile
-          >
+          >-->
+          <!-- AVATAR -->
+          <v-avatar class="profile" color="grey" size="170" tile>
             <v-img src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"></v-img>
           </v-avatar>
-
         </v-col>
 
         <!-- INFO -->
-        <v-col >
-          <v-list-item
-            color="rgba(0, 0, 0, .4)"
-            dark
-          >
+        <v-col>
+          <v-list-item color="rgba(0, 0, 0, .4)" dark>
             <v-list-item-content>
               <v-list-item-title class="title">{{nombres}} {{apellidos}}</v-list-item-title>
               <v-list-item-subtitle>Edad: {{edad}}</v-list-item-subtitle>
@@ -50,60 +28,82 @@
           </v-list-item>
         </v-col>
 
-        <v-col >
+        <v-col>
           <!-- BOTON EDITAR -->
-          <v-btn @click="editarPerfil" type="submit" color="#43A047" dark class="titulo">EDITAR MI PERFIL</v-btn>
+          <v-btn
+            @click="abrirDialogoEditar()"
+            type="submit"
+            color="#43A047"
+            dark
+            class="titulo"
+          >EDITAR MI PERFIL</v-btn>
         </v-col>
       </v-row>
+    </v-card>
 
-      
-   
-  </v-card>
+    <v-card>
+      <v-tabs v-model="tab" background-color="transparent" color="basil" grow>
+        <v-tab v-for="item in items" :key="item.tab">{{ item.tab }}</v-tab>
+      </v-tabs>
 
-  <v-card>
-    <v-tabs
-      v-model="tab"
-       background-color="transparent"
-      color="basil"
-      grow
-    >
-      <v-tab
-        v-for="item in items"
-        :key="item.tab"
-      >
-        {{ item.tab }}
-      </v-tab>
-    </v-tabs>
+      <v-tabs-items v-model="tab">
+        <v-tab-item>
+          <v-container fluid>
+            <v-row>
+              <v-col v-for="proy in misProyectos" :key="proy.nombre" cols="12" md="4">
+                <app-proyecto :proyecto="proy"></app-proyecto>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-tab-item>
 
-    <v-tabs-items v-model="tab">
-      <v-tab-item
-       v-for="item in items"
-        :key="item"
-      >
-         <v-container fluid>
-          <v-row>
+        <v-tab-item>
+          <v-container fluid>
+            <v-row>
+              <v-col v-for="proy in misDonaciones" :key="proy.nombre" cols="12" md="4">
+                <app-proyecto :proyecto="proy"></app-proyecto>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-card>
 
-            <!-- TODO, hacer cambio aqui -->
-            <v-col   
-              v-for="proy in misProyectos" :key="proy.nombre"
-              cols="12"
-              md="4"
-            >
 
-            <app-proyecto :proyecto="proy"></app-proyecto>
+    <v-dialog v-model="dialogoEditar" persistent max-width="600px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">Edita tu proyecto:</span>
+        </v-card-title>
 
-              <!-- <v-img
-                :src="`https://picsum.photos/500/300?image=${i * n * 5 + 10}`"
-                :lazy-src="`https://picsum.photos/10/6?image=${i * n * 5 + 10}`"
-                aspect-ratio="1"
-              ></v-img> -->
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-tab-item>
-    </v-tabs-items>
-  </v-card>
-</div>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field outlined v-model="nuevoNombres" label="Nombres" required></v-text-field>
+                <v-text-field outlined v-model="nuevoApellidos" label="Apellidos" required></v-text-field>
+                <v-text-field outlined v-model="nuevaEdad" label="Edad" required></v-text-field>
+                <v-text-field outlined v-model="nuevoCiudad" label="Ciudad" required></v-text-field>
+                <v-text-field outlined v-model="nuevaProfesion" label="Profesión" required></v-text-field>
+                <v-text-field outlined v-model="nuevoCorreo" label="Correo Electrónico" required></v-text-field>
+                <v-text-field outlined v-model="nuevoSitiosWeb" label="Sitios Web" required></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="#A51F1F" text @click="dialogoEditar = false">Cancelar</v-btn>
+          <v-btn color="#2D7C39" text @click="editarPerfil()">Terminar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+
+
+  </div>
 </template>
 
 <script>
@@ -112,7 +112,7 @@ import axios from "../plugins/axios";
 
 export default {
   components: {
-   appProyecto: Proyecto
+    appProyecto: Proyecto
   },
   data() {
     return {
@@ -123,17 +123,24 @@ export default {
       contrasena: "",
       edad: 0,
       profesion: "",
-      ciudad:"",
+      ciudad: "",
       sitiosWeb: "",
       misProyectos: [],
       misDonaciones: [],
       cont: 0,
       tab: null,
-      items: [
-          { tab: 'MIS PROYECTOS' },
-          { tab: 'MIS DONACIONES' },
-      ]
+      items: [{ tab: "MIS PROYECTOS" }, { tab: "MIS DONACIONES" }],
+      dialogoEditar: false,
 
+
+      nuevoNombres: "",
+      nuevoApellidos: "",
+      nuevaEdad: 0,
+      nuevoCiudad: "",
+      nuevaProfesion: "",
+      nuevoCorreo:"",
+      nuevaContrasena:"",
+      nuevoSitiosWeb:""
     };
   },
   computed: {
@@ -166,49 +173,100 @@ export default {
     },
     misDonaciones_usu() {
       return this.$store.state.misDonaciones;
-    },
-    
+    }
   },
   created() {
-    (this.id = this.id_usu),
-    (this.nombres = this.nombres_usu),
-    (this.apellidos = this.apellidos_usu),
-    (this.correo = this.correo_usu),
-    (this.edad = this.edad_usu),
-    (this.profesion = this.profesion_usu),
-    (this.ciudad = this.ciudad_usu),
-    (this.sitiosWeb = this.sitiosWeb_usu),
+      (this.id = this.id_usu),
+      (this.nombres = this.nombres_usu),
+      (this.apellidos = this.apellidos_usu),
+      (this.correo = this.correo_usu),
+      (this.edad = this.edad_usu),
+      (this.profesion = this.profesion_usu),
+      (this.ciudad = this.ciudad_usu),
+      (this.sitiosWeb = this.sitiosWeb_usu),
 
-    console.log(this.misProyectos_usu);
+      (this.nuevoNombres = this.nombres_usu),
+      (this.nuevoApellidos = this.apellidos_usu),
+      (this.nuevoCorreo = this.correo_usu),
+      (this.nuevaEdad = this.edad_usu),
+      (this.nuevaProfesion = this.profesion_usu),
+      (this.nuevoCiudad = this.ciudad_usu),
+      (this.nuevoSitiosWeb = this.sitiosWeb_usu),
+
+      console.log(this.misProyectos_usu);
 
     this.misProyectos_usu.forEach(element => {
       console.log(element);
-      axios.get("/proyecto/"+element+"").then(response => {
-       if (response.status == 200) {
-         this.misProyectos.push(response.data);
-
-       }
-      })
+      axios.get("/proyecto/" + element + "").then(response => {
+        if (response.status == 200) {
+          this.misProyectos.push(response.data);
+        }
+      });
     });
 
     this.misDonaciones_usu.forEach(element => {
       console.log(element);
-      axios.get("/proyecto/"+element+"").then(response => {
-       if (response.status == 200) {
-         this.misDonaciones.push(response.data);
-
-       }
-      })
+      axios.get("/proyecto/" + element + "").then(response => {
+        if (response.status == 200) {
+          this.misDonaciones.push(response.data);
+        }
+      });
     });
 
-      
+
+
   },
-  methods:{
-    editarPerfil(){
+  methods: {
+    abrirDialogoEditar(){
+      this.dialogoEditar = true;
+    },
+    editarPerfil() {
+
+      if (isNaN(this.edad)) {
+        alert(
+          "El campo de edad debe ser numérico."
+        );
+        this.nuevaEdad= this.edad;
+      } else {
+        let usuarioEditado = {
+          nombres: this.nuevoNombres,
+          apellidos: this.nuevoApellidos,
+          edad: this.nuevaEdad,
+          profesion:this.nuevaProfesion,
+          sitiosWeb:this.nuevoSitiosWeb,
+          ciudad:this.nuevoCiudad,
+          correo:this.nuevoCorreo,
+        };
+        axios
+          .put("/usuario/" + this.id, usuarioEditado)
+          .then(response => {
+            if (response.status == 200) {
+              alert("El usuario se ha editado");
+              this.nombres = this.nuevoNombres;
+              this.apellidos = this.nuevoApellidos;
+              this.edad = this.nuevaEdad;
+              this.profesion = this.nuevaProfesion;
+              this.sitiosWeb = this.nuevoSitiosWeb;
+              this.ciudad = this.nuevoCiudad;
+              this.correo = this.nuevoCorreo;
+
+
+                this.$store.commit("changeTheNombres",this.nuevoNombres);
+                this.$store.commit("changeTheApellidos", this.nuevoApellidos);
+                this.$store.commit("changeTheCorreo", this.nuevoCorreo);
+                this.$store.commit("changeTheEdad", this.nuevaEdad);
+                this.$store.commit("changeTheProfesion", this.nuevaProfesion);
+                this.$store.commit("changeTheCiudad", this.nuevoCiudad);
+                this.$store.commit("changeTheSitiosWeb",this.nuevoSitiosWeb);
+
+            }
+          });
+      }
+      this.dialogoEditar = false;
+
 
     }
   }
-
 };
 </script>
 
